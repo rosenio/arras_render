dcb060b73c783dd0d3bdef3a1d1157fe952bdb5b
rosenio <kenio3d@gmail.com>
2024-09-19 13:03:44 -0300

    update

--- cmake/ArrasRenderCompileOptions.cmake
+++ cmake/ArrasRenderCompileOptions.cmake
@@ -41,7 +41,9 @@ function(ArrasRender_cxx_compile_options target)
             PRIVATE
                 -march=core-avx2                # Specify the name of the target architecture
                 -fdelayed-template-parsing      # Shader.h has a template method that uses a moonray class which is no available to scene_rdl2 and is only used in moonray+
-                /W3    # disable auto_ptr deprecated warnings from log4cplus-1.
+                $<$<CXX_COMPILER_ID:MSVC>:/W3>
+                $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wno-deprecated-declarations>
+                $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wno-unused-value>    # disable auto_ptr deprecated warnings from log4cplus-1.
                 -Wno-unused-value               # caused by opt-debug build and MNRY_VERIFY.
         )
     elseif (CMAKE_CXX_COMPILER_ID STREQUAL Intel)
